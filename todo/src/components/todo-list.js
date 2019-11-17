dfb1ca9ac2ab5878728bf1c03a88174453e8b959
19Al85 <shapofall@gmail.com>
2019-11-17 17:10:26 +0200

    Array and Spread operator

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import TodoListItem from './todo-list-item';
+import React from "react";
+import TodoListItem from "./todo-list-item";
 
-const TodoList=()=>{
-    return (
-        <ul>
-            <li><TodoListItem label="Drink coffee"/></li>
-            <li><TodoListItem 
-                label="Build React App"
-                important/></li>
-        </ul>
-    )
-}
+const TodoList = ({ todos }) => {
+  const elements = todos.map(item => {
+      return(
+        <li>
+            {/* using Spread operator */}
+            <TodoListItem {...item}/>
+
+            {/* with out Spread operator */}
+            {/* <TodoListItem 
+                label={item.label} 
+                important={item.important} /> */}
+        </li>
+      )
+  });
+
+  return (
+    <ul>
+        {elements}
+    </ul>
+  );
+};
 
 export default TodoList;
