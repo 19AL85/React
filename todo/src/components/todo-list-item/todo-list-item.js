961165a15023b809c02daa3230806695efc19301
19Al85 <shapofall@gmail.com>
2019-11-17 23:49:20 +0200

    Learning State (aded important and done behavior)

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,43 +1,78 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import "./todo-list-item.css";
 
 // Компонент класс
-export default class TodoListItem extends Component{
-
+export default class TodoListItem extends Component {
   // constructor(params) {
-  //     super();
-  //     this.onLabelClick=()=>{
-  //       console.log(this.props.label)
-  //     }
+  //   super();
+  //   this.onLabelClick = () => {
+  //     console.log(this.props.label);
+  //   };
+  //   this.state = {
+  //     done: false
+  //   };
   // }
 
-  onLabelClick=()=>{
-    console.log(this.props.label)
-  }
+  //-----эксперементальный синтаксис еще не вошедший в стандарт ES
 
-  render(){
+  state = {
+    done: false,
+    important: false
+  };
 
-    const {label, important}=this.props;
+  onLabelClick = () => {
+    this.setState(({done})=>{
+      return{
+        done: !done
+      }
+    });
+  };
 
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
-  
+  onMarkImportant = () => {
+    /* если state не зависит от текущего состояния
+    this.setState({
+      important: true
+    })
+    */
+    this.setState(state => {
+      return{
+        important: !state.important
+      };
+    });
+  };
+  //---------------------------------------
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
+
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" 
-          style={style}
-          onClick={this.onLabelClick}>
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
-  
-        <button type="button" className="btn btn-outline-success btn-sm float-right">
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
+        >
           <i className="fa fa-exclamation" />
         </button>
-  
-        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
           <i className="fa fa-trash-o" />
         </button>
       </span>
