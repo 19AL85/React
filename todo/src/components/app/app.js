be603afafd00624fe9b09a580235e24ae4838857
19Al85 <shapofall@gmail.com>
2019-11-18 12:24:09 +0200

    Delete element  added

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from "../app-header"
 import SearchPanel from "../search-panel";
@@ -7,24 +7,50 @@ import ItemStatusFilter from "../item-status-filter";
 
 import "./app.css";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Make Awesom App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 }
-  ];
+export default class App extends Component {
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Make Awesom App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      // Здесь меняется состояние напрямую (splice - изменяет текущий масив) чего делать нельзя!!
+      // todoData.splice(idx,1);
+      // return{
+      //   todoData:todoData
+      // };
+
+      const beforeIdx = todoData.slice(0, idx);
+      const afterIdx = todoData.slice(idx + 1);
+
+      const newData = [...beforeIdx, ...afterIdx];
+
+      return {
+        todoData: newData
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData}
+          onDeleted={(id) => this.deleteItem(id)} />
       </div>
-      <TodoList todos={todoData}
-        onDeleted={(id) => console.log('del', id)} />
-    </div>
-  );
+    );
+  }
+
 };
 
-export default App;
